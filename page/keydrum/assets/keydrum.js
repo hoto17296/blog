e54381993e933be4f4ccb173e23fcd01eae3ebd8
hoto <hoto17296@gmail.com>
2013-08-25 00:29:43 +0900

    change sound

--- page/keydrum/assets/keydrum.js
+++ page/keydrum/assets/keydrum.js
@@ -1,20 +1,19 @@
 $(function(){
-  var dir = 'sound/shino/mp3/';
+  var dir = 'sound/kb/mp3/';
   var ext = 'mp3';
   var sounds = {
-    'kick':            { color: '0056b9' },
-    'snare':           { color: 'ab47cb' },
-    'snare_low':       { color: 'eaa8ff' },
-    'snare_cr':        { color: 'a3d771' },
-    'snare_or':        { color: 'a3d771' },
-    'high_tom':        { color: '489dff' },
-    'mid_tom':         { color: '0a7bff' },
-    'floor_tom':       { color: '016fde' },
-    'ohh_edge':        { color: 'ff5817' },
-    'chh_edge':        { color: 'ff7964' },
-    'crash_cymbal-l':  { color: 'ffbf4d' },
-    'crash_cymbal-r':  { color: 'ffbf4d' },
-    'china_cymbal':    { color: 'ff5817' },
+    'kick':           { color: '0056b9' },
+    'snare':          { color: 'ab47cb' },
+    'snare_low':      { color: 'eaa8ff' },
+    'sticks':         { color: 'a3d771' },
+    'tom_high':       { color: '489dff' },
+    'tom_mid':        { color: '0a7bff' },
+    'tom_low':        { color: '016fde' },
+    'hihat_open':     { color: 'ff5817' },
+    'hihat_closed':   { color: 'ff7964' },
+    'cymbal_crash_l': { color: 'ffbf4d' },
+    'cymbal_crash_r': { color: 'ffbf4d' },
+    'cymbal_chinese': { color: 'ff5817' },
   };
   var map = {
      32: 'kick', // space
@@ -25,39 +24,39 @@ $(function(){
      66: 'snare_low', // b
      78: 'snare_low', // n
      74: 'snare_low', // j
-     67: 'snare_cr', // c
-     77: 'snare_or', // m
-     53: 'high_tom', // 5
-     54: 'high_tom', // 6
-     82: 'high_tom', // r
-     84: 'high_tom', // t
-     89: 'high_tom', // y
-     56: 'mid_tom', // 8
-     57: 'mid_tom', // 9
-     85: 'mid_tom', // u
-     73: 'mid_tom', // i
-     79: 'mid_tom', // o
-     75: 'floor_tom', // k
-     76: 'floor_tom', // l
-    186: 'floor_tom', // +
-    188: 'floor_tom', // <
-    190: 'floor_tom', // >
-     65: 'ohh_edge', // a
-     83: 'ohh_edge', // s
-     68: 'chh_edge', // d
-     90: 'chh_edge', // z
-     88: 'chh_edge', // x
-     50: 'crash_cymbal-l', // 2
-     51: 'crash_cymbal-l', // 3
-     81: 'crash_cymbal-l', // q
-     87: 'crash_cymbal-l', // w
-     69: 'crash_cymbal-l', // e
-    189: 'crash_cymbal-r', // =
-    187: 'crash_cymbal-r', // ~
-     80: 'crash_cymbal-r', // p
-    219: 'crash_cymbal-r', // @{
-    221: 'crash_cymbal-r', // }
-     13: 'china_cymbal', // Enter
+     67: 'sticks', // c
+     77: 'sticks', // m
+     53: 'tom_high', // 5
+     54: 'tom_high', // 6
+     82: 'tom_high', // r
+     84: 'tom_high', // t
+     89: 'tom_high', // y
+     56: 'tom_mid', // 8
+     57: 'tom_mid', // 9
+     85: 'tom_mid', // u
+     73: 'tom_mid', // i
+     79: 'tom_mid', // o
+     75: 'tom_low', // k
+     76: 'tom_low', // l
+    186: 'tom_low', // +
+    188: 'tom_low', // <
+    190: 'tom_low', // >
+     65: 'hihat_open', // a
+     83: 'hihat_open', // s
+     68: 'hihat_closed', // d
+     90: 'hihat_closed', // z
+     88: 'hihat_closed', // x
+     50: 'cymbal_crash_l', // 2
+     51: 'cymbal_crash_l', // 3
+     81: 'cymbal_crash_l', // q
+     87: 'cymbal_crash_l', // w
+     69: 'cymbal_crash_l', // e
+    189: 'cymbal_crash_r', // =
+    187: 'cymbal_crash_r', // ~
+     80: 'cymbal_crash_r', // p
+    219: 'cymbal_crash_r', // @{
+    221: 'cymbal_crash_r', // }
+     13: 'cymbal_chinese', // Enter
   };
 
   var objects = {};
